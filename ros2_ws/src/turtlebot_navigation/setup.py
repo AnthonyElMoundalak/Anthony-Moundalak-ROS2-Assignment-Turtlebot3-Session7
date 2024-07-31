3d0a5bfa71f445f010b9af8edff18d76837e53ff
Anthony El Moundalak <anthony.moundalak@net.usj.edu.lb>
2024-07-31 23:52:17 +0300

    Add launcher file and adjust setup.py and package.xml files

--- ros2_ws/src/turtlebot_navigation/setup.py
+++ ros2_ws/src/turtlebot_navigation/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/my_robot_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +21,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'robot_driver = turtlebot_navigation.robot_driver:main',
+            'wall_finder_service = turtlebot_navigation.wall_finder_service:main',
+            'lap_time_action_server = turtlebot_navigation.lap_time_action_server:main',
+            'lap_time_action_client = turtlebot_navigation.lap_time_action_client:main',
         ],
     },
 )
